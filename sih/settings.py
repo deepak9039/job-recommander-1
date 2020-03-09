7ed0b4024b40e6cb7766de20250453ca3072d205
abhishek kushwahha <abhishekushwaha610@users.noreply.github.com>
2020-03-09 18:39:52 +0530

    gitignore added

--- sih/settings.py
+++ sih/settings.py
@@ -75,22 +75,22 @@ WSGI_APPLICATION = 'sih.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'sql12326913',
+        'USER': 'sql12326913',
+        'PASSWORD': 'hYSZQghwjf',
+        'HOST': 'sql12.freesqldatabase.com',
+        'PORT': '3306',
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'django',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
 
 
 # Password validation
