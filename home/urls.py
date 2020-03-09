13c7db2a8a907693a729126b7e0270d794b938f1
abhishek kushwahha <abhishekushwaha610@users.noreply.github.com>
2020-03-09 17:35:34 +0530

    profile info
    profile info backend

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
  
 urlpatterns = [
-     path('',views.home,name='home'),
-     path('signup',views.signup,name='signup'),
-     path('profile/',views.login,name='login')
+    path('',views.home,name='home'),
+    path('signup',views.signup,name='signup'),
+    path('profile/',views.login,name='login')
  ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, HttpResponse
-
+from django.contrib import auth
+from django.contrib.auth.models import User
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 
 def signup(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        
+        user = User.objects.create_user(username=username,password=password,email=email,first_name=name)
+        user.save()
+        auth.login(request,user)
+        param = {
+            "user" : user
+        }
+        return render(request,"profile.html",param)
     return render(request,'signup.html')
 
 def login(request):
